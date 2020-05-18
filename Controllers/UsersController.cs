53c806fde370a17f6a3bf631aea2f6a4d4681227
James Ku <jameskutw@gmail.com>
2020-05-18 17:45:59 -0400

    Update entity version

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -40,8 +40,9 @@ namespace WebApi.Controllers
         {
             var user = _userService.Authenticate(model.Username, model.Password);
 
-            if (user == null)
+            if (user == null) {
                 return BadRequest(new { message = "Username or password is incorrect" });
+            }
 
             var tokenHandler = new JwtSecurityTokenHandler();
             var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
